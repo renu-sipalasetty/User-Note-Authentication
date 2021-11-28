dc7a140aa00aac67157203b9536db1988cee98ef
renu-sipalasetty <renukabhavanis.work@gmail.com>
2021-11-28 12:51:42 +0530

    updated user authentication

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import App from './App'
+import configureStore from './Store/configureStore'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store=configureStore()
+console.log(store.getState())
+store.subscribe(()=>{
+    console.log('updated state',store.getState())
+})
+ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('root'))
